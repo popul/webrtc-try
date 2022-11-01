bf56a5ae81bd42de74c367040fa9262b76956906
Paul Musso <paul@musso.io>
2022-11-01 13:02:38 +0000

    Update 4 files

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require("http");
 
 //create a server object:
 http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
+  .createServer(function (req, res) {
+    res.write("Hello World 2!"); //write a response to the client
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
